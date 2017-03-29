fbba2fe794b972281b571596d3180d3ef3678adb
zerojimmy <919286317@qq.com>
2017-03-29 11:53:47 +0800

    1

---
+++ main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"TextViewAndWebViewSampleAppDelegate");
+    [pool release];
+    return retVal;
+}
